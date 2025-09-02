80c59958d2eab755253f5eb4938ab6d9c9d3c6f9
curtisqt30 <143355730+curtisqt30@users.noreply.github.com>
2025-09-01 19:48:57 -0700

    feat(login): Added Forgot Password link
    - Added "Forgot Password?" link under password input in LoginScreen.tsx

--- frontend/app/screens/LoginScreen.tsx
+++ frontend/app/screens/LoginScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 import FormButton from "@/components/ui/FormButton";
 
 export default function LoginScreen() {
@@ -7,16 +8,20 @@ export default function LoginScreen() {
   const [password, setPassword] = useState("");
 
   const handleLogin = () => {
+    // Check for empty fields
     if (!email.trim() || !password.trim()) {
       alert("Please enter both email and password.");
       return;
     }
+    // Login logic here
     console.log("Logging in with:", email, password);
+    // TODO: integrate backend login account
+
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome!</Text>
+      <Text style={styles.title}>Welcome Back!</Text>
       <Text style={styles.subtitle}>Please enter your account here</Text>
 
       <TextInput
@@ -36,8 +41,21 @@ export default function LoginScreen() {
         secureTextEntry
       />
 
-      {/* Replaced TouchableOpacity with FormButton */}
+      <View style={styles.forgotContainer}>
+        <Link href="/forgot-password" asChild>
+          <Text style={styles.forgotLink}>Forgot password?</Text>
+        </Link>
+      </View>
+
       <FormButton title="Login" onPress={handleLogin} />
+
+      <Text style={styles.newUserText}>
+        New user?{" "}
+        <Link href="/signup" asChild>
+          <Text style={styles.link}>Sign up here</Text>
+        </Link>
+      </Text>
+
     </View>
   );
 }
@@ -75,5 +93,26 @@ const styles = StyleSheet.create({
     fontFamily: "Ubuntu_400Regular",
     backgroundColor: "#fff",
     color: "#121212",
+  },
+  forgotContainer: {
+    width: "100%",
+    alignItems: "flex-end",
+    marginBottom: 10
+    ,
+  },
+  forgotLink: {
+    color: "#8A5CF6",
+    fontFamily: "Ubuntu_500Medium",
+    fontSize: 14,
+  },
+  link: {
+    color: "#8A5CF6",
+    fontFamily: "Ubuntu_500Medium",
+  },
+  newUserText: {
+    marginTop: 15,
+    color: "#B8B8B8",
+    fontFamily: "Ubuntu_400Regular",
+    fontSize: 14,
   },
 });
