6fcff13e696a86ec716a78c28ce542d0204420fc
curtisqt30 <143355730+curtisqt30@users.noreply.github.com>
2025-09-17 16:19:50 -0700

    chore(auth): Fixed typos in code
    - Fixed unintentional typo in security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -4,12 +4,12 @@ import bycrypt
 # Functions to hash and verify passwords
 def hash_password(password: str) -> str:
     """Hash a password for storing."""
-    salt = bycryptt.gensalt()
-    hashed = bycryptt.hashpw(password.encode('utf-8'), salt)
+    salt = bycrypt.gensalt()
+    hashed = bycrypt.hashpw(password.encode('utf-8'), salt)
     return hashed.decode('utf-8')
 
 def verify_password(password: str, hashed: str) -> bool:
     """Verify a stored password against one provided by user"""
-    return bycryptt.checkpw(password.encode('utf-8'), hashed.encode('utf-8'))
+    return bycrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8'))
 
 # add more security-related functions here as needed
