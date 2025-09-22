77e98e6bbc68287d9807a96233dd2b414aea8c22
curtisqt30 <143355730+curtisqt30@users.noreply.github.com>
2025-09-21 18:11:45 -0700

    fix(ui): Added handling when no previous page exists for Back Button
    - Added a handle to ensure that if user's have no previous page exist, the backbutton defaults to /home

--- frontend/components/ui/BackButton.tsx
+++ frontend/components/ui/BackButton.tsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { TouchableOpacity, StyleSheet } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import { useRouter } from "expo-router";
+import { useRouter, useSegments } from "expo-router";
 import { DarkTheme as Colors } from "@/components/ui/ColorPalette";
 
-// A back button component to navigate to the previous screen
 export default function BackButton() {
   const router = useRouter();
+  const segments = useSegments();
+
+  const handlePress = () => {
+    if (segments.length > 1) {
+      // Normal case
+      router.back();
+    } else {
+      // No previous page
+      router.replace("/home");
+    }
+  };
 
   return (
-    <TouchableOpacity style={styles.container} onPress={() => router.back()}>
+    <TouchableOpacity style={styles.container} onPress={handlePress}>
       <Ionicons name="arrow-back" size={24} color={Colors.textPrimary} />
     </TouchableOpacity>
   );
