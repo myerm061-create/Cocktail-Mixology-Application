e3074f29b757486a2ee8f04175114d4d5a7aa2ef
curtisqt30 <143355730+curtisqt30@users.noreply.github.com>
2025-10-08 15:47:01 -0700

    chore(linting): Ran ESLint Fix command
    - Reduced the 43 problems to 29 problems (7 errors, 22 warnings)

--- frontend/components/ExternalLink.tsx
+++ frontend/components/ExternalLink.tsx
@@ -1,4 +1,5 @@
-import { Href, Link } from 'expo-router';
+import type { Href} from 'expo-router';
+import { Link } from 'expo-router';
 import { openBrowserAsync } from 'expo-web-browser';
 import { type ComponentProps } from 'react';
 import { Platform } from 'react-native';
