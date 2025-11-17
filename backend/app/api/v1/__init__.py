cb259d11ca44c2fe8a921b2f75636df166ed9952
curtisqt30 <143355730+curtisqt30@users.noreply.github.com>
2025-11-17 15:14:33 -0800

    fix(imports): fix imports, should fix failing tests

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -1,15 +1,20 @@
 from fastapi import APIRouter
 
-from . import routes_auth
-from . import routes_auth as routes_auth_users
-from . import routes_auth_email, routes_health
-from . import routes_assistant
-from . import routes_recommendations
+from . import (
+    routes_assistant,
+    routes_auth,
+    routes_auth_account,
+    routes_auth_email,
+    routes_health,
+    routes_ingredients,
+    routes_recommendations,
+)
 
 api_v1 = APIRouter()
 api_v1.include_router(routes_auth.router)
 api_v1.include_router(routes_auth_email.router)
+api_v1.include_router(routes_auth_account.router)
 api_v1.include_router(routes_health.router)
-api_v1.include_router(routes_auth_users.router)
 api_v1.include_router(routes_assistant.router)
+api_v1.include_router(routes_ingredients.router)
 api_v1.include_router(routes_recommendations.router)
