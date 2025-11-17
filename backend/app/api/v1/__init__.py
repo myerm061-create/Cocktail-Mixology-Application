9d9181668027193f2c0050c6677c5104de489fed
curtisqt30 <143355730+curtisqt30@users.noreply.github.com>
2025-11-16 23:15:56 -0800

    chore(api): fixed imports for backend endpoints

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -3,9 +3,13 @@ from fastapi import APIRouter
 from . import routes_auth
 from . import routes_auth as routes_auth_users
 from . import routes_auth_email, routes_health
+from . import routes_assistant
+from . import routes_recommendations
 
 api_v1 = APIRouter()
 api_v1.include_router(routes_auth.router)
 api_v1.include_router(routes_auth_email.router)
 api_v1.include_router(routes_health.router)
 api_v1.include_router(routes_auth_users.router)
+api_v1.include_router(routes_assistant.router)
+api_v1.include_router(routes_recommendations.router)
